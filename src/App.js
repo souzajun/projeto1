7ad60069199d85532f274a11308b58c00a461bef
Francisco Costa <souzajun@hotmail.com>
2024-03-22 21:57:14 -0300

    comsumo de api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AlbumPhotos = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  const fetchPhotos = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+      setPhotos(response.data);
+      
+    } catch (error) {
+      console.error('Foto não localizada:', error);
+    }
+  };
 
 
-function App() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React</h1>
-      </header>
+    <body>
+    <div class="col-sm-12">
+      <h2 id="titulo1">Photos</h2>
+      {photos.map(photo => (
+        <div id="titulo2 "class='caroussel cartao card' key={photo.id}>
+          <img src={photo.thumbnailUrl} alt={photo.title} title={photo.title} />
+          Foto: {photo.id}
+      </div>
+      ))}
     </div>
+    </body>
   );
-}
+};
 
-export default App;
+export default AlbumPhotos;
